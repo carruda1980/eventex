2d2f763952766c2d1a3ea109b140c4bf3e2051bb
Carlos Arruda <caugustogarruda@gmail.com>
2020-10-29 14:06:37 -0300

    Test html tags subscribe form

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -2,12 +2,21 @@ from django.test import TestCase
 
 
 class SubscriptionsTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
+
     def test_get(self):
         """GET /inscricao/ must return status code 200"""
-        response = self.client.get('/inscricao/')
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(200, self.resp.status_code)
 
     def test_template(self):
         """GET /inscricao/ must use subscriptions/subscription_form.html"""
-        response = self.client.get('/inscricao/')
-        self.assertTemplateUsed(response, 'subscriptions/subscription_form.html')
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
+
+    def test_html(self):
+        """Subscribe form must contain html tags"""
+        self.assertContains(self.resp, '<form')
+        self.assertContains(self.resp, '<input', 5)
+        self.assertContains(self.resp, 'type="text"', 3)
+        self.assertContains(self.resp, 'type="email"', 1)
+        self.assertContains(self.resp, 'type="submit')
