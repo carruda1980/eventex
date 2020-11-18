c6d0c998d7806d56dd8558a49aa452816bf09c20
caugustoarruda <caugustogarruda@gmail.com>
2020-11-18 13:45:35 -0300

    setUp test

--- eventex/subscriptions/tests/test_admin.py
+++ eventex/subscriptions/tests/test_admin.py
@@ -5,45 +5,40 @@ from eventex.subscriptions.admin import SubscriptionModelAdmin, Subscription, ad
 
 
 class SubscriptionModelAdminTest(TestCase):
-    def test_has_action(self):
-        """Action mark_as_paid should be installed."""
-        model_admin = SubscriptionModelAdmin(Subscription, admin.site)
-        self.assertIn('mark_as_paid', model_admin.actions)
-
-    def test_mark_all(self):
-        """It should mark all selected subscriptions as paid"""
+    def setUp(self):
         Subscription.objects.create(
             name='Carlos Arruda', cpf='12345678911',
             email='caugustogarruda@gmail.com', phone='31996840810'
         )
-        queryset = Subscription.objects.all()
 
-        model_admin = SubscriptionModelAdmin(Subscription, admin.site)
+        self.model_admin = SubscriptionModelAdmin(Subscription, admin.site)
+
+    def test_has_action(self):
+        """Action mark_as_paid should be installed."""
+        self.assertIn('mark_as_paid', self.model_admin.actions)
+
+    def test_mark_all(self):
+        """It should mark all selected subscriptions as paid"""
+        queryset = Subscription.objects.all()
         mock = Mock()
         old_message_user = SubscriptionModelAdmin.message_user
-        model_admin.message_user = mock
+        self.model_admin.message_user = mock
 
-        model_admin.mark_as_paid(None, queryset)
-
+        self.model_admin.mark_as_paid(None, queryset)
 
         self.assertEqual(1, Subscription.objects.filter(paid=True).count())
         SubscriptionModelAdmin.message_user = old_message_user
 
     def test_message(self):
         """It should send a messa to the user"""
-        Subscription.objects.create(
-            name='Carlos Arruda', cpf='12345678911',
-            email='caugustogarruda@gmail.com', phone='31996840810'
-        )
-        queryset = Subscription.objects.all()
 
-        model_admin = SubscriptionModelAdmin(Subscription, admin.site)
+        queryset = Subscription.objects.all()
 
         mock = Mock()
         old_message_user = SubscriptionModelAdmin.message_user
-        model_admin.message_user = mock
+        self.model_admin.message_user = mock
 
-        model_admin.mark_as_paid(None, queryset)
+        self.model_admin.mark_as_paid(None, queryset)
 
         mock.assert_called_once_with(None, '1 inscrição foi marcada como paga.')
 
