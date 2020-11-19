cbaee4de4099687f773e5bb191a773e6e35e1b5e
caugustoarruda <caugustogarruda@gmail.com>
2020-11-19 19:30:14 -0300

    Test cpf is digit

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -10,3 +10,15 @@ class SubscriptionFormTest(TestCase):
         """Form must have 4 fields"""
         expected = ['name', 'cpf', 'email', 'phone']
         self.assertSequenceEqual(expected, list(self.form.fields))
+
+    def test_cpf_is_digits(self):
+        """CPF must only accept digits. """
+        data = dict(
+            name='Carlos Arruda', cpf='abcd5678901',
+            email='caugustogarruda@gmail.com', phone='31996840810'
+        )
+
+        form = SubscriptionForm(data)
+        form.is_valid()
+
+        self.assertListEqual(['cpf'], list(form.errors))
